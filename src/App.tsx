c56d01503fbc38e6cde16d785d12f98bcac31e63
Sergei Zheleznov <sksdes@gmail.com>
2020-12-26 22:42:39 +0100

    Add react app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-xs">
+      <header className="App-header">
+        hi there!
+      </header>
+    </div>
+  );
+}
+
+export default App;
